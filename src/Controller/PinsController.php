33a4513b6d0baafc46b92b64de650590cb266c82
OusmaneMballo <ousmanemballo576@gmail.com>
2020-05-20 19:29:29 +0000

    Create and read dans la table pins

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -2,18 +2,38 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpKernel\DataCollector\DumpDataCollector;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Pin;
 
 class PinsController extends AbstractController
 {
+    private $em;
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em=$em;
+    }
+
     /**
      * @Route("/")
      */
     public function index()
     {
+
+        $pinReposi=$this->em->getRepository(Pin::class);
+
+         $pin=new Pin();
+         /*$pin->setTitre("Titre2");
+        $pin->setDescription("Article2");
+        $this->em->persist($pin);
+        $this->em->flush();*/
+        $pins=$pinReposi->findAll();
+
         return $this->render('pins/index.html.twig', [
-            'controller_name' => 'PinsController',
+            'listPins' => $pins,
         ]);
     }
 }
